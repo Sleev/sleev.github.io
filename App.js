70262b385b9d77a00d914715dbbbb9c163d7518b
Skyler Kidd <f.s.kidd@gmail.com>
2015-10-25 13:58:23 -0700

    site structure

---
+++ App.js
@@ -0,0 +1,31 @@
+(function() {
+
+  "use strict";
+
+  var App = angular.module("App", [
+    "App.controllers",
+    "App.services",
+    "App.directives",
+    "App.filters",
+    "ngRoute",
+    "ngResource"
+  ]);
+
+  App.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'view/home.html',
+        controller: 'HomeCtrl'
+      })
+      // .when('/about', {
+      //      templateUrl: 'view/about.html',
+      //      controller: 'AboutCtrl'
+      // })
+      // .when('/contact', {
+      //      templateUrl: 'view/contact.html',
+      //      controller: 'ContactCtrl'
+      // })
+      .otherwise({redirectTo : '/'});
+  });
+
+}());
---
+++ controller/controllers.js
@@ -0,0 +1,20 @@
+(function() {
+
+  "use strict";
+
+  var App = angular.module("App.controllers", []);
+
+
+  App.controller("HomeCtrl", ["$scope", function ($scope){
+
+  }]);
+
+  App.controller("AboutCtrl", ["$scope", function ($scope){
+
+  }]);
+
+  App.controller("ContactCtrl", ["$scope", function ($scope){
+
+  }]);
+
+}());
